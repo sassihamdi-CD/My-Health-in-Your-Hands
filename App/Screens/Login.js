b68de8046517c56ed1025fe6e9908bec330514dd
Paul <tunmisejayeoba@gmai.com>
2024-02-08 04:14:36 +0100

    Frontend development

--- App/Screens/Login.js
+++ App/Screens/Login.js
@@ -1,53 +1,58 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import app from './../../assets/images/app_logo.png'
 import Colors from '../../assets/Shared/Colors'
+import { Dimensions } from 'react-native'
+
 export default function Login() {
   return (
-     <View style={{alignItems:'center'}}>
-       <Image source={app_logo}
-       style={styles.appImage}
-       />
-       <View style={{backgroundColor:Colors.white
-         padding:25,
-         alignItems:'center',
-         marginTop:-50,
-         borderTopLeftRadius:20,
-         borderTopRightRadius:20,
-
-   }}>
-         <Text style={style.heading}> Your Utimate Doctor <Text/>
-         <Text style={style.heading}> Appointment medical Booking App <Text/>
-         <Text> Book your appointment effortlessly with Hamndi </Text>
-         <TouchableOpacity 
-         onPress={()=>console.log("Clicked")}
-         style={{padding:16,
-         backgroundColor:Colour.PRIMARY,
-         borderRadius:98,
-         alignItems:'center',
-         MarginTop:20,
-         width:Dimensions.get('screen').width*0.8,
-         }}>
-           <Text style={{frontsize:17,color:Colors.white}}> Login with Google </Text>
-         </TouchableOpacity> 
-      </View> 
-   </View> 
+    <View style={{ alignItems: 'center' }}>
+      <Image source={app}
+        style={styles.appImage}
+      />
+      <View style={
+        {
+          backgroundColor: Colors.white,
+          padding: 25,
+          alignItems: 'center',
+          marginTop: -50,
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+        }}>
+        <Text style={styles.heading}> Your Utimate Doctor 
+          <Text> Appointment medical Booking App </Text>
+          <Text> Book your appointment effortlessly with Hamndi </Text>
+        </Text>
+        <TouchableOpacity
+          onPress={() => console.log("Clicked")}
+          style={
+            {
+              padding: 16,
+              backgroundColor: Colors.PRIMARY,
+              borderRadius: 98,
+              alignItems: 'center',
+              marginTop: 20,
+              width: Dimensions.get('screen').width * 0.8,
+            }}>
+          <Text style={{ fontSize: 17, color: Colors.white }}> Login with Google </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  appImage:{
-    width:300,
-    height:500,
-    objectfit:'contain',
-    marginTop:50
-    borderRadius:20,
-    borderwidth:6,
-    bordercolor:'#000'
+  appImage: {
+    width: 300,
+    height: 500,
+    objectFit: 'contain',
+    marginTop: 50,
+    borderRadius: 20,
+    borderWidth: 6,
+    borderColor: '#000'
   },
-    heading:{
-         frontsize:28 ,
-         fontWeight:"bold"
-       }
-
-})  
+  heading: {
+    fontSize: 28,
+    fontWeight: "bold"
+  }
+})
